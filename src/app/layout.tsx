1865dda256317a725e0002e852e4f3fafa84955d
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2025-02-21 15:53:33 +0200

    Initialize Next.js project with basic structure and configuration

---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from "next";
+import "./globals.scss";
+
+export const metadata: Metadata = {
+  title: "BookNexus",
+  description:
+    "Discover, explore, and track your favorite books with BookNexus. Your go-to platform for book lovers to find recommendations, reviews, and personalized reading lists.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
