2cc7f5de7ac5415fb80f2ffe606c9c1d7a110373
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2025-02-21 17:41:54 +0200

    Added data for sidebar items

---
+++ src/components/Sidebar/sidebarItems.ts
@@ -0,0 +1,23 @@
+import {
+  FaCompass,
+  FaThLarge,
+  FaBook,
+  FaHeart,
+  FaListAlt,
+  FaCog,
+  FaQuestionCircle,
+  FaSignOutAlt,
+} from "react-icons/fa";
+
+const sidebarItems = [
+  { title: "Discover", icon: FaCompass },
+  { title: "Category", icon: FaThLarge },
+  { title: "My Library", icon: FaBook },
+  { title: "Favorites", icon: FaHeart },
+  { title: "Wishlist", icon: FaListAlt },
+  { title: "Settings", icon: FaCog },
+  { title: "Help", icon: FaQuestionCircle },
+  { title: "Logout", icon: FaSignOutAlt },
+];
+
+export default sidebarItems;
