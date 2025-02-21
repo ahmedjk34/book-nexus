d60c1cfbbb7edfae0e8c03e7cfd0a36cdd63f9bf
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2025-02-21 18:18:06 +0200

    Implemented .active class to highlight the current path in sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,9 +1,15 @@
+"use client";
 import React from "react";
+import Link from "next/link";
 import styles from "./side-bar.module.scss";
 import sidebarItems from "./sidebarItems";
+import { usePathname } from "next/navigation";
 type Props = {};
 
 function Sidebar({}: Props) {
+  const path = usePathname(); // Get the current path using usePathname()
+  console.log(path);
+
   return (
     <div className={styles.sidebar}>
       <h2 className={styles.logo}>Book Nexus</h2>
@@ -11,22 +17,34 @@ function Sidebar({}: Props) {
         <h3>Menu</h3>
         <div className={styles.menuGroup}>
           {sidebarItems.slice(0, 5).map((item) => (
-            <div key={item.title} className={styles.menuItem}>
+            <Link
+              key={item.title}
+              href={item.link}
+              className={`${styles.menuItem} ${
+                path === item.link ? styles.active : ""
+              }`}
+            >
               <div className={styles.iconContainer}>
                 {React.createElement(item.icon)}
               </div>
               <span>{item.title}</span>
-            </div>
+            </Link>
           ))}
         </div>
         <div className={styles.menuGroup}>
           {sidebarItems.slice(5).map((item) => (
-            <div key={item.title} className={styles.menuItem}>
+            <Link
+              key={item.title}
+              href={item.link}
+              className={`${styles.menuItem} ${
+                path === item.link ? styles.active : ""
+              }`}
+            >
               <div className={styles.iconContainer}>
                 {React.createElement(item.icon)}
               </div>
               <span>{item.title}</span>
-            </div>
+            </Link>
           ))}
         </div>
       </div>
