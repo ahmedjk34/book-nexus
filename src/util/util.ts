68029225005b24304a63b75de386b76d4a431781
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2025-02-23 14:07:11 +0200

    Add function to generate a random offset within a specified range

--- src/util/util.ts
+++ src/util/util.ts
@@ -3,3 +3,8 @@ export const extractPath = (str: string): string => {
   const url = new URL(str);
   return url.pathname;
 };
+
+//create a function to generate a certain offset of range 0 to a given parameter
+export const getRandomOffset = (max: number): number => {
+  return Math.floor(Math.random() * max);
+};
