42fd17b206ce58e8e60542ffd4bf9c273ea875e0
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2025-02-23 15:26:49 +0200

    Add BookCategory component to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "./_homeComponents/Hero";
 import styles from "../styles/pages/home-page.module.scss";
+import BookCategory from "./_homeComponents/BookCategory";
 export default function Home() {
   return (
     <div className={styles.homePage}>
       <Hero />
+      <BookCategory />
     </div>
   );
 }
