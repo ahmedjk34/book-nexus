8d8591556ba121d4c09687100aa34b68d5e7b23c
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2025-02-24 12:16:26 +0200

    Add utility functions for string case conversion

--- src/util/util.ts
+++ src/util/util.ts
@@ -8,3 +8,17 @@ export const extractPath = (str: string): string => {
 export const getRandomOffset = (max: number): number => {
   return Math.floor(Math.random() * max);
 };
+
+export const convertFromSnakeToCapitalCase = (input: string): string => {
+  return input
+    .split("_")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+};
+
+export const convertTOSnakeCase = (input: string): string => {
+  return input
+    .split(" ")
+    .map((word) => word.toLowerCase())
+    .join("_");
+};
