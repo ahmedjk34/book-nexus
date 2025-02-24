ada60f328cf270e7b52e26699e7603ee022b2494
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2025-02-24 12:16:53 +0200

    Add static data file & added  book subjects

---
+++ src/util/staticData.ts
@@ -0,0 +1,20 @@
+const bookSubjects = [
+  "fiction",
+  "nonfiction",
+  "fantasy",
+  "mystery",
+  "romance",
+  "science_fiction",
+  "history",
+  "self_help",
+  "philosophy",
+  "psychology",
+  "biography",
+  "children",
+  "science",
+  "technology",
+  "art",
+  "education",
+];
+
+export { bookSubjects };
