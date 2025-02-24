80533699413652be2da3f77b7051a07b2d34ee87
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2025-02-24 18:49:39 +0200

    Add API function to fetch random book quotes

--- src/util/API.ts
+++ src/util/API.ts
@@ -3,6 +3,7 @@ import { getRandomOffset } from "./util";
 
 const BASE_URL = "https://openlibrary.org";
 
+//OpenLibrary API
 export async function getPopularBooks(): Promise<any> {
   try {
     const response = await axios.get(
@@ -11,6 +12,19 @@ export async function getPopularBooks(): Promise<any> {
     return response.data.docs;
   } catch (error) {
     console.error("Error fetching popular books:", error);
+    throw error;
+  }
+}
+
+//random-book-quote API
+export async function getQuoteOfTheDay(): Promise<any> {
+  try {
+    const response = await axios.get(
+      "https://random-book-quote-api.ahmedtaher212005.workers.dev/quote/today"
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching random book quote:", error);
     throw error;
   }
 }
