73e853dacc2ae7d30453445dd33ce8b1bfccee61
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2025-02-26 10:46:34 +0200

    Add redirect for category path without slug

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,16 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["covers.openlibrary.org"],
   },
+  async redirects() {
+    return [
+      // redirect in case of category path being without slug (page number)
+      {
+        source: "/category",
+        destination: "/category/1",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
