bb10871269cb8705090047d4097061017c8792ca
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2025-03-02 20:57:07 +0200

    Add AuthNav component to exports in index file

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 import Sidebar from "./Sidebar/Sidebar";
 import Searchbar from "./Searchbar/Searchbar";
 import BookCard from "./BookCard/BookCard";
+import AuthNav from "./AuthNav/AuthNav";
 
-export { Sidebar, Searchbar, BookCard };
+export { Sidebar, Searchbar, BookCard, AuthNav };
