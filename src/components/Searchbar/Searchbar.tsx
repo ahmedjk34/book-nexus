e5dc935f45566a992a61d899355fc5d291210d54
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2025-03-02 16:21:19 +0200

    Implement search functionality

--- src/components/Searchbar/Searchbar.tsx
+++ src/components/Searchbar/Searchbar.tsx
@@ -1,19 +1,30 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { FaSearch } from "react-icons/fa";
 import styles from "./search-bar.module.scss";
+import { useRouter } from "next/navigation";
 
 type Props = {};
 
 function Searchbar({}: Props) {
+  const [query, setQuery] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    router.push(`/search/${query}`);
+  };
+
   return (
-    <form className={styles.searchbar}>
+    <form className={styles.searchbar} onSubmit={handleSubmit}>
       <div className={styles.categoryComboBox}>All categories</div>
       <FaSearch className={styles.searchbarIcon} size={20} />
       <input
         type="text"
         placeholder="Find a book you'd like..."
         className={styles.searchbarInput}
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
       />
       <button className={styles.searchbarButton} type="submit">
         Search
