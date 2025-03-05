06ceed0728b57d17e5bb2a7efd6a679919840b73
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2025-03-05 20:33:21 +0200

    Refactor wishlist API to use new Request and Response patterns; add POST method for updating wishlist

--- src/app/api/[userId]/wishlist/route.ts
+++ src/app/api/[userId]/wishlist/route.ts
@@ -1,15 +1,44 @@
 import prisma from "@/lib/prisma";
 import { NextApiRequest, NextApiResponse } from "next";
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
-  const { userId } = req.query;
+export async function GET(
+  req: Request,
+  { params }: { params: Promise<{ userId: string }> }
+) {
+  const { userId } = await params;
 
   try {
     const wishlist = await prisma.wishlist.findUnique({
       where: { userId: Number(userId) },
     });
-    res.status(200).json(wishlist);
+    return new Response(JSON.stringify(wishlist), { status: 200 });
   } catch (error) {
-    res.status(500).json({ error: "Internal Server Error" });
+    return new Response(JSON.stringify({ error: "Internal Server Error" }), {
+      status: 500,
+    });
+  }
+}
+
+export async function POST(
+  req: Request,
+  { params }: { params: Promise<{ userId: string }> }
+) {
+  const { userId } = await params;
+  const { bookId } = await req.json();
+
+  try {
+    const wishlist = await prisma.wishlist.update({
+      where: { userId: Number(userId) },
+      data: {
+        books: {
+          push: bookId,
+        },
+      },
+    });
+    return new Response(JSON.stringify(wishlist), { status: 200 });
+  } catch (error) {
+    return new Response(JSON.stringify({ error: "Internal Server Error" }), {
+      status: 500,
+    });
   }
 }
