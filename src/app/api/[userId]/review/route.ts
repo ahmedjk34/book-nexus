9d0171f1253da6293468c4352a37438205c6981c
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2025-03-08 19:08:05 +0200

    Add POST API route to create reviews

--- src/app/api/[userId]/review/route.ts
+++ src/app/api/[userId]/review/route.ts
@@ -1,14 +1,14 @@
 import prisma from "@/lib/prisma";
-import { isNumericalValue } from "@/util/util";
+import { isEmptyFields, isNumericalValue } from "@/util/util";
 import { NextRequest } from "next/server";
 
 export async function GET(
   req: NextRequest,
   { params }: { params: { userId: string } }
 ) {
-  try {
-    const { userId } = params;
+  const { userId } = params;
 
+  try {
     if (!isNumericalValue(Number(userId))) {
       return Response.json({ error: "Invalid userId" }, { status: 400 });
     }
@@ -20,5 +20,35 @@ export async function GET(
     return Response.json(reviews, { status: 200 });
   } catch (error) {
     return Response.json({ error: "Failed to fetch reviews" }, { status: 500 });
+  }
+}
+
+export async function POST(
+  req: NextRequest,
+  { params }: { params: { userId: string } }
+) {
+  const { userId } = params;
+  const { content, rating, bookId } = await req.json();
+  if (isEmptyFields(bookId, rating, userId)) {
+    return Response.json(
+      { error: "Some required fields are empty" },
+      { status: 400 }
+    );
+  }
+  if (!isNumericalValue(Number(userId))) {
+    return Response.json({ error: "Invalid userId" }, { status: 400 });
+  }
+  try {
+    const review = await prisma.review.create({
+      data: {
+        content: content || "",
+        rating,
+        userId: Number(userId),
+        bookId,
+      },
+    });
+    return Response.json(review, { status: 201 });
+  } catch (error) {
+    return Response.json({ error: "Failed to add review" }, { status: 500 });
   }
 }
