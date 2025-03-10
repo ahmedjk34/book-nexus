07f880f9bc046d69ec1de0732f207af7cced437d
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2025-03-10 17:38:20 +0200

    Updated the book Image component to have the loading be eager or lazy based if the image in view port or no

--- src/components/BookCard/BookImage.tsx
+++ src/components/BookCard/BookImage.tsx
@@ -1,13 +1,33 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Image from "next/image";
 import styles from "./book-card.module.scss";
 
 const BookImage = ({ src, alt }: { src: string; alt: string }) => {
   const [isLoading, setIsLoading] = useState(true);
+  const [isInViewport, setIsInViewport] = useState(false);
+  const imageRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsInViewport(true);
+          observer.disconnect(); // Stop observing once in viewport
+        }
+      },
+      { rootMargin: "100px", threshold: 0.5 } // Start loading slightly before it's visible
+    );
+
+    if (imageRef.current) {
+      observer.observe(imageRef.current);
+    }
+
+    return () => observer.disconnect();
+  }, []);
 
   return (
-    <div className={styles.imageContainer}>
+    <div ref={imageRef} className={styles.imageContainer}>
       {isLoading && <div className={styles.imageLoadingOverlay}></div>}
       <Image
         src={src}
@@ -15,8 +35,8 @@ const BookImage = ({ src, alt }: { src: string; alt: string }) => {
         className={styles.bookCover}
         sizes="285px"
         fill
-        priority
         onLoad={() => setIsLoading(false)}
+        loading={isInViewport ? "lazy" : "eager"}
       />
     </div>
   );
