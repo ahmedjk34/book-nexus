3f6c5bd97f443ab4a7a3df4926d8dddecbd8fbff
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2025-03-10 17:38:58 +0200

    Extracted & Modified styles to add an image loading overlay

--- src/components/BookCard/BookCard.tsx
+++ src/components/BookCard/BookCard.tsx
@@ -1,28 +1,21 @@
 import React from "react";
 import styles from "./book-card.module.scss";
 import { DetailedBook, SimpleBook } from "@/util/Types";
-import Image from "next/image";
 import { isDetailedBook, processBookAuthors } from "@/util/util";
 import Link from "next/link";
+import BookImage from "./BookImage";
 
 type Props = {
   book: SimpleBook | DetailedBook;
 };
 
 function BookCard({ book }: Props) {
-  console.log(book.key);
+  const imageName = isDetailedBook(book) ? book.cover_id : book?.cover_i;
+  const bookImageURL = `https://covers.openlibrary.org/b/id/${imageName}-L.jpg`;
+
   return (
     <Link className={styles.bookCard} href={`/book/${book.key.split("/")[2]}`}>
-      <Image
-        src={`https://covers.openlibrary.org/b/id/${
-          isDetailedBook(book) ? book.cover_id : book?.cover_i
-        }-L.jpg`}
-        alt={book.title}
-        className={styles.bookCover}
-        sizes="285px"
-        fill
-        priority
-      />
+      <BookImage src={bookImageURL} alt={book.title} />
       <div className={styles.noBookCoverOverlay}>No Book Cover Found</div>
       <div className={styles.bookOverlay}>
         <h3>{book.title}</h3>
