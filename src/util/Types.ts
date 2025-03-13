f0f15df4b6525ea08c530b17b7802e48c79fd9d7
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2025-03-13 21:25:07 +0200

    Added Edition type

--- src/util/Types.ts
+++ src/util/Types.ts
@@ -67,3 +67,40 @@ export interface DetailedBook {
   created?: { type: string; value: string };
   last_modified?: { type: string; value: string };
 }
+
+export interface Edition {
+  type: {
+    key: string;
+  };
+  key: string;
+  title: string;
+  full_title: string;
+  authors: Array<{
+    key: string;
+  }>;
+  isbn_13: string[];
+  languages: Array<{
+    key: string;
+  }>;
+  pagination: string;
+  publish_date: string;
+  publishers: string[];
+  source_records: string[];
+  subjects: string[];
+  works: Array<{
+    key: string;
+  }>;
+  covers: number[];
+  number_of_pages: number;
+  weight: string;
+  latest_revision: number;
+  revision: number;
+  created: {
+    type: string;
+    value: string;
+  };
+  last_modified: {
+    type: string;
+    value: string;
+  };
+}
