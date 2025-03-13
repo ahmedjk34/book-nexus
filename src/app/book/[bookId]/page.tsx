61ddb7afa92af57038fbfdede5bcd9011a712856
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2025-03-13 21:50:31 +0200

    Redirect page if the user only entered the workId (it gets the most popular edition and passes its edition id)

---
+++ src/app/book/[bookId]/page.tsx
@@ -0,0 +1,17 @@
+import { getMostPopularEdition } from "@/util/API";
+import { redirect } from "next/navigation";
+import React from "react";
+
+type Props = {
+  params: Promise<{ bookId: string }>;
+};
+
+//The whole purpose of this page is to redirect if the user only entered a work id
+//! Add a more intuitive way with proper loading!
+async function page({ params }: Props) {
+  const { bookId } = await params;
+  const mostPopularEdition = await getMostPopularEdition(bookId);
+  return redirect(`/book/${bookId}/${mostPopularEdition?.key.split("/")[2]}`);
+}
+
+export default page;
