476b580ef7718e06bd5d6807cd6b0be08df0bfd2
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2025-03-14 22:52:42 +0200

    Refactored Book Card to use the new format of data returned

--- src/components/BookCard/BookCard.tsx
+++ src/components/BookCard/BookCard.tsx
@@ -1,17 +1,16 @@
 import React from "react";
 import styles from "./book-card.module.scss";
-import { DetailedBook, SimpleBook } from "@/util/Types";
-import { isDetailedBook, processBookAuthors } from "@/util/util";
+import { SimpleBook } from "@/util/Types";
+import { processBookAuthors } from "@/util/util";
 import Link from "next/link";
 import BookImage from "./BookImage";
 
 type Props = {
-  book: SimpleBook | DetailedBook;
+  book: SimpleBook;
 };
 
 function BookCard({ book }: Props) {
-  const imageName = isDetailedBook(book) ? book.cover_id : book?.cover_i;
-  const bookImageURL = `https://covers.openlibrary.org/b/id/${imageName}-L.jpg`;
+  const bookImageURL = `https://covers.openlibrary.org/b/id/${book.cover_id}-L.jpg`;
 
   return (
     <Link className={styles.bookCard} href={`/book/${book.key.split("/")[2]}`}>
@@ -19,12 +18,7 @@ function BookCard({ book }: Props) {
       <div className={styles.noBookCoverOverlay}>No Book Cover Found</div>
       <div className={styles.bookOverlay}>
         <h3>{book.title}</h3>
-        <h4>
-          Written by:{" "}
-          {isDetailedBook(book)
-            ? processBookAuthors(book.authors)
-            : processBookAuthors(book.author_name)}
-        </h4>
+        <h4>Written by: {processBookAuthors(book.authors)}</h4>
       </div>
     </Link>
   );
