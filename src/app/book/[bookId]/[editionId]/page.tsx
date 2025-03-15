06e642e269e03cd5997a2cfceba8903d86feef7c
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2025-03-15 20:35:02 +0200

    Added MainInfo section

--- src/app/book/[bookId]/[editionId]/page.tsx
+++ src/app/book/[bookId]/[editionId]/page.tsx
@@ -1,15 +1,33 @@
+import { getFullBookInfo } from "@/util/API";
+import { DetailedBook } from "@/util/Types";
 import React from "react";
+import styles from "../../../../styles/pages/book-page.module.scss";
+import MainInfo from "./_components/MainInfo";
 
 type Props = {
   params: Promise<{ bookId: string; editionId: string }>;
 };
 
 async function page({ params }: Props) {
-  const par = await params;
-  const { bookId, editionId } = par;
-  console.log(bookId, editionId);
+  const { bookId, editionId } = await params;
+  const book: DetailedBook = await getFullBookInfo(bookId, editionId);
 
-  return <div>page</div>;
+  return (
+    <div className={styles.bookPage}>
+      <div className={styles.pageContainer}>
+        {/*place holder data for the rating & reviews for now*/}
+        <MainInfo
+          title={book.title}
+          cover_id={book.cover_id}
+          authorsNames={book.authors_names}
+          description={book.description}
+          rating={4}
+          numberOfRatings={2421}
+          numberOfReviews={14}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default page;
