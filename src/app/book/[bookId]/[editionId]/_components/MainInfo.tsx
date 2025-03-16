07acf34910e8a384c192b8b443e21fdb5232681f
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2025-03-16 22:29:10 +0200

    Fixed an HMR bug and designed the side info

--- src/app/book/[bookId]/[editionId]/_components/MainInfo.tsx
+++ src/app/book/[bookId]/[editionId]/_components/MainInfo.tsx
@@ -1,8 +1,11 @@
+"use client";
 import React from "react";
 import styles from "../../../../../styles/pages/book-page.module.scss";
-import VerticalBookImage from "@/components/VerticalBookImage/VerticalBookImage";
+import { FaPen, FaPlus, FaArrowDown } from "react-icons/fa6";
+import { VerticalBookImage } from "@/components";
+
 type Props = {
-  cover_id: string;
+  cover_id: number;
   title: string;
   authorsNames: string[];
   rating: number;
@@ -15,17 +18,30 @@ function MainInfo({ cover_id, title }: Props) {
   const bookImageURL = `https://covers.openlibrary.org/b/id/${cover_id}-L.jpg`;
 
   return (
-    <div className={styles.heroSection}>
-      {/* <div className={styles.mainInfoOverlay}>
-        <div />
-        <div />
-      </div> */}
-      <div className={styles.sideInfo}>
-        <VerticalBookImage src={bookImageURL} alt={title} />
+    <div>
+      <div className={styles.heroSection}>
+        <div className={styles.sideInfo}>
+          <div className={styles.sideInfo}>
+            <VerticalBookImage src={bookImageURL} alt={title} />
+            <button>
+              Want to Read <FaPen />
+            </button>
+            <button>
+              Buy on Amazon USA <FaArrowDown />
+            </button>
+            <button>
+              Add to Wishlist <FaPlus />
+            </button>
+          </div>
+        </div>
+        <div className={styles.mainInfo}></div>
       </div>
-      <div className={styles.mainInfo}></div>
     </div>
   );
 }
 
 export default MainInfo;
+
+{
+  /*placeholder value for shop name*/
+}
