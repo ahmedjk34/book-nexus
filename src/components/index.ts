e3669785f4a43880bd0690c05d12275bf6585175
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2025-03-16 22:00:47 +0200

    Added VerticalBookImage to components export

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,6 @@ import Sidebar from "./Sidebar/Sidebar";
 import Searchbar from "./Searchbar/Searchbar";
 import BookCard from "./BookCard/BookCard";
 import AuthNav from "./AuthNav/AuthNav";
+import VerticalBookImage from "./VerticalBookImage/VerticalBookImage";
 
-export { Sidebar, Searchbar, BookCard, AuthNav };
+export { Sidebar, Searchbar, BookCard, AuthNav, VerticalBookImage };
